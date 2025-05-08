835607959c43c7069382e489ab9c3ac489205bd5
David Lewis <david@uniondesign.ca>
2025-05-08 19:58:38 -0300

    Initial Commit

--- assets/js/build/frontend.asset.php
+++ assets/js/build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-dom-ready'), 'version' => '4df33910a12de6294a04');
+<?php return array('dependencies' => array('wp-dom-ready'), 'version' => '4bb367d81d7240d22ba3');
