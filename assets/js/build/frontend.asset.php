6f642bac5c8c463f44e7d3a17bfabdefc866eec8
David Lewis <david@uniondesign.ca>
2025-05-09 14:29:26 -0300

    Testing in Magnet Site

--- assets/js/build/frontend.asset.php
+++ assets/js/build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-dom-ready'), 'version' => 'bcf5f3a6da410dac80c3');
+<?php return array('dependencies' => array('wp-dom-ready'), 'version' => '54a49a27b5e9b7ca874f');
